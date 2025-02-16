2fb08a4af72e4746f5c61e44a3ef8fbf81dfd436
bunrieucuaa <ann71576@gmail.com>
2025-02-16 20:34:51 +0700

    feat: need to review about ref

---
+++ Section8-Refs-Portals/01-starting-project/src/components/Player.jsx
@@ -0,0 +1,45 @@
+import { useState, useRef } from "react";
+
+// export default function Player() {
+//   const [enteredPlayerName, setEnteredPlayerName] = useState("");
+//   const [submitted, setSubmitted] = useState(false);
+
+//   const handleChange = (event) => {
+//     setSubmitted(false);
+//     setEnteredPlayerName(event.target.value);
+//   };
+
+//   function handleClick() {
+//     setSubmitted(true);
+//   }
+
+//   return (
+//     <section id="player">
+//       <h2>Welcome {submitted ? enteredPlayerName : "unknow entity"}</h2>
+//       <p>
+//         <input type="text" onChange={handleChange} value={enteredPlayerName} />
+//         <button onClick={handleClick}>Set Name</button>
+//       </p>
+//     </section>
+//   );
+// }
+
+export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? "unknow entity"}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
